d7795007fdeb6b25c90a17dc04e62aaa674f91a1
Patatozor <Patatozaur@gmail.com>
2015-05-23 18:27:09 +0200

    teststructure mvc admin+ assets

---
+++ admin/view/accueil/home.php
@@ -0,0 +1,11 @@
+w!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css" />
+            <link rel="stylesheet" href="../assets/css/base.css" />
+            <script type="text/javascript" src="../assets/js/jquery-1.11.3.min.js"></script>
+        </head>
+        <body>
+            <p>Hello world</p>
+        </body>
+    </html>
