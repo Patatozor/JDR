5fe199ce17a9595c14f7f7e617bbf1a2b930164a
Patatozor <Patatozaur@gmail.com>
2015-05-23 22:09:25 +0200

    regles ajout

--- admin/view/layout/footer.php
+++ admin/view/layout/footer.php
@@ -2,5 +2,9 @@
         <footer>
             <p>Ceci est un footer</p>
         </footer>
+        <pre>
+            $_POST : <?php var_dump($_POST); ?><br />
+            $title : <?php var_dump($title); ?><br />
+        </pre>
     </body>
 </html>
