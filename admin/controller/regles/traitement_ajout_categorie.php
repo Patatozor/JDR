d12fa0a0259d465418c08a96209f4e6014309314
Patatozor <Patatozaur@gmail.com>
2015-05-23 22:03:55 +0200

    regles ajout

--- admin/controller/regles/traitement_ajout_categorie.php
+++ admin/controller/regles/traitement_ajout_categorie.php
@@ -2,7 +2,7 @@
 include_once('model/regles/ajout_categorie.php');
 $locationerror = 'location:?module=regles&action=ajout_categorie';
 if(isset($_POST['nom_categorie_de_regles'])){
-    if(test_titre($_POST['nom_categorie_de_regles'])){
+    if(test_titre($_POST['nom_categorie_de_regles']&&$_POST['nom_categorie_de_regles']!='')){
         $titre_categorie = $_POST['nom_categorie_de_regles'];
     }else{
         header($locationerror);
