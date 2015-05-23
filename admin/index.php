36946965d8ff05e834d4152ca3c453cf1b369e5a
Patatozor <Patatozaur@gmail.com>
2015-05-23 18:57:35 +0200

    ajout assets

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,10 @@
 <?php
 include_once('../libs/traitement_string.php');
+include_once('../libs/session.php');
 include_once('../config/environnement.php');
-include_once('model/connexion_bdd.php');
-
+if(my_session_start(SESSION)){
+    include_once('model/connexion_bdd.php');
+}
 $module = (isset($_GET['module']))?$_GET['module']:'accueil';
 $action = (isset($_GET['action']))?$_GET['action']:'home';
 $url = 'controller/'.$module.'/'.$action.'.php';
--- config/environnement.php
+++ config/environnement.php
@@ -3,3 +3,6 @@ define('BDD_HOST','localhost');
 define('BDD_MDP','');
 define('BDD_LOGIN','root');
 define('BDD_NAME','jdr');
+define('SESSION', 'jdr');
+define('HEADER','view/header.php');
+define('FOOTER','view/footer.php');
