5c974fef427a9cdcaf0a844c2d82ef521b7e65ea
Flubix <diet.gislain@gmail.com>
2015-05-23 22:10:22 +0200

    Modif description

--- admin/controller/regles/traitement_ajout_categorie.php
+++ admin/controller/regles/traitement_ajout_categorie.php
@@ -5,8 +5,8 @@ if(isset($_POST['nom_categorie_de_regles'])){
     if(test_titre($_POST['nom_categorie_de_regles'])){
         $titre_categorie = $_POST['nom_categorie_de_regles'];
     }
-    if(isset($_POST['desciption_categorie_de_regles'])!=''){
-        $description_categorie_de_regles = $_POST['desciption_categorie_de_regles'];
+    if(isset($_POST['description_categorie_de_regles'])!=''){
+        $description_categorie_de_regles = $_POST['description_categorie_de_regles'];
     }
     if(isset($titre_categorie)&&isset($description_categorie_de_regles)){
         insert_categorie($titre_categorie,$description_categorie_de_regles);
--- admin/view/regles/ajouter_categorie.php
+++ admin/view/regles/ajouter_categorie.php
@@ -3,8 +3,8 @@
     <form method='post' action='?module=regles&action=traitement_ajout_categorie'>
         <label for='nom_categorie_de_regles'>Nom de la catégorie :</label>
         <input type='text' name='nom_categorie_de_regles' id='nom_categorie_de_regles' value=''/>
-        <label for='desciption_categorie_de_regles'>Description de la catégorie :</label>
-        <textarea rows='10' cols='50' name='desciption_categorie_de_regles' id='desciption_categorie_de_regles' placeholder=""></textarea>
+        <label for='description_categorie_de_regles'>Description de la catégorie :</label>
+        <textarea rows='10' cols='50' name='description_categorie_de_regles' id='description_categorie_de_regles' placeholder=""></textarea>
         <input type='submit' value='Envoyer'/>
     </form>
 <?php include_once(FOOTER); ?>
