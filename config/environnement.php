0ff62de9efcc4ece35a023f9f9071c8d7f72ecf0
Patatozor <Patatozaur@gmail.com>
2015-05-23 18:59:05 +0200

    ajout assets

--- config/environnement.php
+++ config/environnement.php
@@ -4,5 +4,5 @@ define('BDD_MDP','');
 define('BDD_LOGIN','root');
 define('BDD_NAME','jdr');
 define('SESSION', 'jdr');
-define('HEADER','view/header.php');
-define('FOOTER','view/footer.php');
+define('HEADER','view/layout/header.php');
+define('FOOTER','view/layout/footer.php');
