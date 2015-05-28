6a5fb46d2bfb9b49bac585a7e4f6afe8abeadf72
Flubix <diet.gislain@gmail.com>
2015-05-28 10:28:10 +0200

    J'ai fait les pages traitement_ajout et ajout pour les regles je te laisse vérifier. J'ai 2 warning sur ajout_regle.php par rapport a ma requete SQL je sais pas ce que c'est

--- admin/controller/regles/traitement_ajout_regle.php
+++ admin/controller/regles/traitement_ajout_regle.php
@@ -1,1 +1,18 @@
 <?php
+include_once('model/regles/ajout_regle.php');
+$location = 'location:?module=regles&action=ajout_regles';
+if(isset($_POST['nom_regles'])){
+    if(test_titre($_POST['nom_regles']&&$_POST['titre_regles']!='')){
+        $nom_regles = $_POST['nom_regles'];
+    }
+    if(isset($_POST['description_regles'])!=''){
+        $description_regles = $_POST['description_regles'];
+    }
+    if(isset($_POST['titre_regles'])!=''){
+        $titre_regles = $_POST['titre_regles'];
+    }
+    if(isset($titre_regles)&&isset($description_regles)&&isset($nom_regles)){
+        insert_regle($nom_regles,$titre_regles,$description_regles);
+    }
+}
+header($location);
--- admin/model/regles/ajout_regle.php
+++ admin/model/regles/ajout_regle.php
@@ -1,1 +1,20 @@
 <?php
+function insert_regle($nom_regles,$titre_regles,$description_regles){
+    global $pdo;
+    try{
+        $query = $pdo->prepare("
+        INSERT INTO regles
+        (nom_regles,titre_regles,description_regles)
+        VALUES
+        (:nom,:titre,:description)
+        ");
+        $query->bindValue(':nom',$nom_regles,PDO::PARAM_STR);
+        $query->bindValue(':titre',$titre_regles,PDO::PARAM_STR);
+        $query->bindValue(':description',$description_regles,PDO::PARAM_STR);
+        $query->execute();
+        return true;
+    }
+    catch(Exception $e){
+        return false;
+    }
+}
